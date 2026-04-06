48f1773eb81a60582d6ff3d780c27499324b27fd
Enrtarr <120576263+Enrtarr@users.noreply.github.com>
2026-04-06 21:07:56 +0200

    Delete iskyjo/src/main directory

--- iskyjo/src/main/java/com/neuilleprime/game/actions/Action.java
+++
@@ -1,7 +0,0 @@
-package com.neuilleprime.game.actions;
-
-import com.neuilleprime.game.GameController;
-
-public interface Action {
-    void execute(GameController game);
-}
--- iskyjo/src/main/java/com/neuilleprime/game/events/GameEventListener.java
+++
@@ -1,7 +0,0 @@
-package com.neuilleprime.game.events;
-
-public interface GameEventListener {
-    void onTurnStarted(TurnStartedEvent event);
-    void onRoundEnded(RoundEndedEvent event);
-    // ...
-}
